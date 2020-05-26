27239e5c37bf8dbcba1b9dee4306b30e22c0ed62
Ustymenko <ustymyar@ukr.net>
2020-05-26 21:20:35 +0300

    26052020

---
+++ WinForm/26052020/26052020/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace _26052020
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Петренко");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Математика");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Степаненко", new System.Windows.Forms.TreeNode[] {
+            treeNode2});
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Іваненко");
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("PE921", 3, 1, new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode3,
+            treeNode4});
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("BE911", 6, 1);
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.buttonADD = new System.Windows.Forms.Button();
+            this.buttonInsert = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonMove = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.CheckBoxes = true;
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.treeView1.ImageIndex = 0;
+            this.treeView1.ImageList = this.imageList1;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            treeNode1.Name = "Node3";
+            treeNode1.SelectedImageIndex = 1;
+            treeNode1.Text = "Петренко";
+            treeNode2.Name = "Математика";
+            treeNode2.Text = "Математика";
+            treeNode3.Name = "Node4";
+            treeNode3.SelectedImageIndex = 1;
+            treeNode3.Text = "Степаненко";
+            treeNode4.Name = "Node2";
+            treeNode4.SelectedImageIndex = 1;
+            treeNode4.Text = "Іваненко";
+            treeNode5.ImageIndex = 3;
+            treeNode5.Name = "Node0";
+            treeNode5.SelectedImageIndex = 1;
+            treeNode5.Text = "PE921";
+            treeNode6.ImageIndex = 6;
+            treeNode6.Name = "Математика";
+            treeNode6.SelectedImageIndex = 1;
+            treeNode6.Text = "BE911";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode5,
+            treeNode6});
+            this.treeView1.SelectedImageIndex = 0;
+            this.treeView1.Size = new System.Drawing.Size(270, 306);
+            this.treeView1.TabIndex = 0;
+            // 
+            // buttonADD
+            // 
+            this.buttonADD.Location = new System.Drawing.Point(364, 14);
+            this.buttonADD.Name = "buttonADD";
+            this.buttonADD.Size = new System.Drawing.Size(75, 23);
+            this.buttonADD.TabIndex = 1;
+            this.buttonADD.Text = "ADD";
+            this.buttonADD.UseVisualStyleBackColor = true;
+            this.buttonADD.Click += new System.EventHandler(this.buttonADD_Click);
+            // 
+            // buttonInsert
+            // 
+            this.buttonInsert.Location = new System.Drawing.Point(445, 14);
+            this.buttonInsert.Name = "buttonInsert";
+            this.buttonInsert.Size = new System.Drawing.Size(75, 23);
+            this.buttonInsert.TabIndex = 2;
+            this.buttonInsert.Text = "Insert";
+            this.buttonInsert.UseVisualStyleBackColor = true;
+            this.buttonInsert.Click += new System.EventHandler(this.buttonInsert_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(364, 43);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.TabIndex = 3;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonMove
+            // 
+            this.buttonMove.Location = new System.Drawing.Point(445, 43);
+            this.buttonMove.Name = "buttonMove";
+            this.buttonMove.Size = new System.Drawing.Size(120, 23);
+            this.buttonMove.TabIndex = 4;
+            this.buttonMove.Text = "Selected for Move";
+            this.buttonMove.UseVisualStyleBackColor = true;
+            this.buttonMove.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(526, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "All checked delete";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(276, 95);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(204, 199);
+            this.listBox1.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(583, 43);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "ToText";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(723, 72);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(145, 199);
+            this.listBox2.TabIndex = 8;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(711, 14);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(106, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "5 ... 1";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(711, 43);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(106, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "1...5";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "agent.png");
+            this.imageList1.Images.SetKeyName(1, "bonus.png");
+            this.imageList1.Images.SetKeyName(2, "budget.png");
+            this.imageList1.Images.SetKeyName(3, "deposit.png");
+            this.imageList1.Images.SetKeyName(4, "kopilka.png");
+            this.imageList1.Images.SetKeyName(5, "privat24.png");
+            this.imageList1.Images.SetKeyName(6, "sms.png");
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(539, 95);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(86, 23);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "find";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(923, 306);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonMove);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonInsert);
+            this.Controls.Add(this.buttonADD);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button buttonADD;
+        private System.Windows.Forms.Button buttonInsert;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonMove;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ WinForm/26052020/26052020/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _26052020
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonADD_Click(object sender, EventArgs e)
+        {
+            TreeNode root = new TreeNode();
+            root.Text = "D123";
+            root.Nodes.Add(new TreeNode("Тимощук", 1, 3));
+            root.Nodes.Add(new TreeNode("Петрук", 1, 4));
+
+            treeView1.Nodes.Add(root);
+            //treeView1.Nodes.Add(root); //error
+
+            TreeNode rootcopy = (TreeNode)root.Clone();
+            treeView1.Nodes.Add(rootcopy);
+        }
+
+        private void buttonInsert_Click(object sender, EventArgs e)
+        {
+            TreeNode node = new TreeNode("Іщук");
+            TreeNode selectNode = treeView1.SelectedNode;
+            selectNode?.Nodes.Insert(-10, node);
+            //treeView1.Nodes[0].Nodes.Insert(11,node);
+            // treeView1.Nodes[1].Nodes.Add(node);
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            //treeView1.Nodes.Remove(treeView1.SelectedNode);
+
+            treeView1.Nodes.RemoveAt(1);
+            treeView1.Focus();
+        }
+
+        private void buttonMove_Click(object sender, EventArgs e)
+        {
+            if (buttonMove.Text == "Selected for Move")
+            {
+                treeView1.Tag = treeView1.SelectedNode;
+                treeView1.Nodes.Remove(treeView1.SelectedNode);
+                buttonMove.Text = "Selected for Insert";
+            }
+            else
+            {
+                treeView1.SelectedNode.Nodes.Add((TreeNode)treeView1.Tag);
+                buttonMove.Text = "Selected for Move";
+                treeView1.Tag = null;
+                treeView1.SelectedNode.Expand();
+                //  treeView1.SelectedNode.Collapse();
+                treeView1.Focus();
+
+            }
+
+            // treeView1.Nodes[1].Nodes.Add(temp);
+        }
+        private void Recursive(TreeNodeCollection nodes, string text = "")
+        {
+            foreach (TreeNode item in nodes)
+            {
+                listBox1.Items.Add(text + item.Text);
+                if (item.Nodes.Count > 0)
+                    Recursive(item.Nodes, text + "\t");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            Recursive(treeView1.Nodes);
+        }
+        private void RecursiveDel(TreeNodeCollection nodes)
+        {
+            var elemdel = nodes.OfType<TreeNode>().Where(x => x.Checked);
+            foreach (var item in elemdel)
+                treeView1.Nodes.Remove(item);
+            foreach (TreeNode item in nodes)
+                RecursiveDel(item.Nodes);
+        }
+      
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RecursiveDel(treeView1.Nodes);
+        }
+
+        private void FtoOne(int n)//5
+        {
+            if (n < 1) return;
+            listBox2.Items.Add(n);
+            FtoOne(n - 1);
+        }
+        private void OnetoN(int n)//5
+        {
+            //if (n > 0)
+            //{
+            //    OnetoN(n - 1);
+            //    listBox2.Items.Add(n);
+            //}
+            if (n < 1) return;
+            OnetoN(n - 1);
+            listBox2.Items.Add(n);
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            FtoOne(5);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            OnetoN(5);
+        }
+        private void RecursiveFind(TreeNodeCollection nodes, string text)
+        {
+            foreach (TreeNode item in nodes)
+            {
+                if (item.Text == text)
+                    listBox1.Items.Add(item);
+                if (item.Nodes.Count > 0)
+                    RecursiveFind(item.Nodes, text);
+            }
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            string textfind = "Петренко";//"BE911";
+            //var arr = treeView1.Nodes.Find(textfind, true);
+            //foreach (var item in arr)
+            //    listBox1.Items.Add(item);
+
+            RecursiveFind(treeView1.Nodes, textfind);
+        }
+    }
+}
