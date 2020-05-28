0cc869b9065fe5f1247861ffd762294f7ca85d64
Ustymenko <ustymyar@ukr.net>
2020-05-28 21:23:28 +0300

    21052020

---
+++ WinForm/21052020/21052020/Form1.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace _21052020
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.buttonColor = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.buttonFolder = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonFont = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.pageSetupDialog1 = new System.Windows.Forms.PageSetupDialog();
+            this.buttonPageSetup = new System.Windows.Forms.Button();
+            this.buttonPrint = new System.Windows.Forms.Button();
+            this.buttonPreview = new System.Windows.Forms.Button();
+            this.printDialog1 = new System.Windows.Forms.PrintDialog();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.SuspendLayout();
+            // 
+            // colorDialog1
+            // 
+            this.colorDialog1.AnyColor = true;
+            // 
+            // buttonColor
+            // 
+            this.buttonColor.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonColor.Location = new System.Drawing.Point(1, 38);
+            this.buttonColor.Name = "buttonColor";
+            this.buttonColor.Size = new System.Drawing.Size(140, 47);
+            this.buttonColor.TabIndex = 0;
+            this.buttonColor.Text = "Select Color";
+            this.buttonColor.UseVisualStyleBackColor = true;
+            this.buttonColor.Click += new System.EventHandler(this.buttonColor_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.SelectedPath = "C:\\Users\\Ustymenko\\Desktop";
+            this.folderBrowserDialog1.ShowNewFolderButton = false;
+            // 
+            // buttonFolder
+            // 
+            this.buttonFolder.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonFolder.Location = new System.Drawing.Point(147, 38);
+            this.buttonFolder.Name = "buttonFolder";
+            this.buttonFolder.Size = new System.Drawing.Size(140, 47);
+            this.buttonFolder.TabIndex = 1;
+            this.buttonFolder.Text = "Select Folder";
+            this.buttonFolder.UseVisualStyleBackColor = true;
+            this.buttonFolder.Click += new System.EventHandler(this.buttonFolder_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(432, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.WordWrap = false;
+            // 
+            // buttonFont
+            // 
+            this.buttonFont.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonFont.Location = new System.Drawing.Point(293, 38);
+            this.buttonFont.Name = "buttonFont";
+            this.buttonFont.Size = new System.Drawing.Size(140, 47);
+            this.buttonFont.TabIndex = 3;
+            this.buttonFont.Text = "Select Font";
+            this.buttonFont.UseVisualStyleBackColor = true;
+            this.buttonFont.Click += new System.EventHandler(this.buttonFont_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(453, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Hello!!!";
+            // 
+            // fontDialog1
+            // 
+            this.fontDialog1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.fontDialog1.MaxSize = 20;
+            this.fontDialog1.MinSize = 10;
+            this.fontDialog1.ShowApply = true;
+            this.fontDialog1.ShowColor = true;
+            this.fontDialog1.Apply += new System.EventHandler(this.fontDialog1_Apply);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "File txt|*.txt|File docx|*.docx|all file|*.*";
+            this.openFileDialog1.Multiselect = true;
+            this.openFileDialog1.Title = "Test file open";
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonOpen.Location = new System.Drawing.Point(559, 38);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(118, 47);
+            this.buttonOpen.TabIndex = 5;
+            this.buttonOpen.Text = "Open File";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(683, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(127, 351);
+            this.listBox1.TabIndex = 6;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonSave.Location = new System.Drawing.Point(439, 38);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(114, 47);
+            this.buttonSave.TabIndex = 7;
+            this.buttonSave.Text = "Save File";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "txt";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(0, 144);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(683, 207);
+            this.textBox2.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(23, 91);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(73, 47);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(111, 91);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(73, 47);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // pageSetupDialog1
+            // 
+            this.pageSetupDialog1.Document = this.printDocument1;
+            this.pageSetupDialog1.ShowNetwork = false;
+            // 
+            // buttonPageSetup
+            // 
+            this.buttonPageSetup.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonPageSetup.Location = new System.Drawing.Point(201, 91);
+            this.buttonPageSetup.Name = "buttonPageSetup";
+            this.buttonPageSetup.Size = new System.Drawing.Size(120, 47);
+            this.buttonPageSetup.TabIndex = 11;
+            this.buttonPageSetup.Text = "Page Setup";
+            this.buttonPageSetup.UseVisualStyleBackColor = true;
+            this.buttonPageSetup.Click += new System.EventHandler(this.buttonPageSetup_Click);
+            // 
+            // buttonPrint
+            // 
+            this.buttonPrint.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonPrint.Location = new System.Drawing.Point(327, 91);
+            this.buttonPrint.Name = "buttonPrint";
+            this.buttonPrint.Size = new System.Drawing.Size(78, 47);
+            this.buttonPrint.TabIndex = 12;
+            this.buttonPrint.Text = "Print";
+            this.buttonPrint.UseVisualStyleBackColor = true;
+            this.buttonPrint.Click += new System.EventHandler(this.buttonPrint_Click);
+            // 
+            // buttonPreview
+            // 
+            this.buttonPreview.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonPreview.Location = new System.Drawing.Point(415, 91);
+            this.buttonPreview.Name = "buttonPreview";
+            this.buttonPreview.Size = new System.Drawing.Size(138, 47);
+            this.buttonPreview.TabIndex = 13;
+            this.buttonPreview.Text = "Preview";
+            this.buttonPreview.UseVisualStyleBackColor = true;
+            this.buttonPreview.Click += new System.EventHandler(this.buttonPreview_Click);
+            // 
+            // printDialog1
+            // 
+            this.printDialog1.AllowCurrentPage = true;
+            this.printDialog1.AllowSelection = true;
+            this.printDialog1.AllowSomePages = true;
+            this.printDialog1.Document = this.printDocument1;
+            this.printDialog1.ShowNetwork = false;
+            this.printDialog1.UseEXDialog = true;
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(810, 351);
+            this.Controls.Add(this.buttonPreview);
+            this.Controls.Add(this.buttonPrint);
+            this.Controls.Add(this.buttonPageSetup);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonFont);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonFolder);
+            this.Controls.Add(this.buttonColor);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button buttonColor;
+        private System.Windows.Forms.Button buttonFolder;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button buttonFont;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.PageSetupDialog pageSetupDialog1;
+        private System.Windows.Forms.Button buttonPageSetup;
+        private System.Windows.Forms.Button buttonPrint;
+        private System.Windows.Forms.Button buttonPreview;
+        private System.Windows.Forms.PrintDialog printDialog1;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+    }
+}
+
---
+++ WinForm/21052020/21052020/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _21052020
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ColorDialog colorDialog = new ColorDialog();
+        private void buttonColor_Click(object sender, EventArgs e)
+        {
+            //if (colorDialog1.ShowDialog() == DialogResult.OK)
+            //    BackColor = colorDialog1.Color;
+            //ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+                BackColor = colorDialog.Color;
+
+        }
+
+        private void buttonFolder_Click(object sender, EventArgs e)
+        {
+            // folderBrowserDialog1.RootFolder = Environment.SpecialFolder.ProgramFiles;
+            //folderBrowserDialog1.SelectedPath =  "C:\\Users\\Ustymenko\\Desktop";
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+            //  BackColor = colorDialog.Color;
+        }
+
+        private void buttonFont_Click(object sender, EventArgs e)
+        {
+            if (fontDialog1.ShowDialog() == DialogResult.OK)
+                fontDialog1_Apply(sender, e);
+        }
+
+        private void fontDialog1_Apply(object sender, EventArgs e)
+        {
+            label1.Font = fontDialog1.Font;
+            label1.ForeColor = fontDialog1.Color;
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+
+            if (openFileDialog1.ShowDialog() ==  DialogResult.OK)
+            {
+                // textBox1.Text = openFileDialog1.FileName;
+
+                switch (openFileDialog1.FilterIndex)
+                {
+                    case 1:
+                        textBox1.Text = "txt";
+                        break;
+                    case 2:
+                        textBox1.Text = "docx";
+                        break;
+                    default:
+                        textBox1.Text = "all";
+                        break;
+                }
+
+                listBox1.Items.Clear();
+                listBox1.Items.AddRange(openFileDialog1.FileNames);
+            }
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = saveFileDialog1.FileName;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                textBox2.Text = File.ReadAllText(openFileDialog1.FileName);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(saveFileDialog1.FileName, textBox2.Text);
+
+            }
+        }
+
+        private void buttonPageSetup_Click(object sender, EventArgs e)
+        {
+            if (pageSetupDialog1.ShowDialog() == DialogResult.OK)
+            {
+                printDocument1.PrinterSettings = pageSetupDialog1.PrinterSettings;
+                printDocument1.DefaultPageSettings = pageSetupDialog1.PageSettings;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pageSetupDialog1.PrinterSettings = new System.Drawing.Printing.PrinterSettings();
+            pageSetupDialog1.PageSettings = new System.Drawing.Printing.PageSettings();
+        }
+
+        string textToPrint;
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            var gr = e.Graphics;
+            gr.MeasureString(textToPrint, textBox2.Font,e.MarginBounds.Size,
+                StringFormat.GenericTypographic,
+                out int charOnPage, out int linesOnPage );
+
+            gr.DrawString(textToPrint, textBox2.Font, Brushes.Red,
+                e.MarginBounds, StringFormat.GenericTypographic);
+
+            textToPrint = textToPrint.Substring(charOnPage);
+
+            e.HasMorePages = textToPrint.Length > 0;
+
+            if (!e.HasMorePages) textToPrint = textBox2.Text;
+        }
+
+        private void buttonPrint_Click(object sender, EventArgs e)
+        {
+            if (printDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textToPrint = textBox2.Text;
+                printDocument1.Print();
+            }
+        }
+
+        private void buttonPreview_Click(object sender, EventArgs e)
+        {
+            textToPrint = textBox2.Text;
+            printPreviewDialog1.Document = printDocument1;
+            printPreviewDialog1.ShowDialog();
+        }
+    }
+}
